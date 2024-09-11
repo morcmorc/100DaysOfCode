375121426100fb681ddb810427109cafbba42f07
bwilliant <marc.stoehr@web.de>
2024-09-12 00:37:08 +0200

    verschachtelte dicts

--- Day19 anfang/main.py
+++ Day19 anfang/main.py
@@ -8,3 +8,22 @@
 # 20.08 i cant
 # 21.08 valo
 # 22.08 immer noch pause :$
+
+food = {
+    "fruechte": {
+        "banane": 12,
+        "birne": 10,
+        "apfel":9,
+    },
+    "gemuese": {
+        "gurke": 2,
+        "salat": 5,
+        "kartoffel": 7,
+    },
+    "fleisch":{
+        "huhn": 10,
+        "fisch": 12,
+        "schwein": 16,
+    }
+}
+print(food["gemuese"]["gurke"])
